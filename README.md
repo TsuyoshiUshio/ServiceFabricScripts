dc3dff498db6c27dfd909471b49291daccef03e8
Tsuyoshi Ushio <tsushi@microsoft.com>
2016-11-17 13:34:32 +0900

    Initial Commit

---
+++ README.md
@@ -0,0 +1,36 @@
+Service Fabric Scripts
+===
+
+These are Scripts for Service Fabric.
+
+1. A KeyVault Creator with Self-Signed Certificate
+---
+
+If you want to have a Secure cluster, you need to type a lot of PowerShell commands
+for KeyValut with X509 certificate.
+However, for the hackathon/demo purpose, you might use Self-Signed Certificate and
+don't want to type a lot of PowerShell commands.
+
+In this case, you can use SignedCertWithKeyVault.ps1.
+
+Usage:
+
+Edit `parameter.json`
+
+```
+PS> Login-AzureRmAccount
+./SignedCertWithKeyVault.ps1 
+
+---------------------------------------------------------
+Done! You've got a KeyVault with Self-Signed Certificate!
+---------------------------------------------------------
+Source Key vault
+/subscriptions/xxxxxxx-xxxxx-xxxxxx6cc9/resourceGroups/IdealKeyGroup/providers/Microsoft.KeyVault/vaults/idealVault
+Certificate URL
+https://idealvault.vault.azure.net:443/secrets/SfCluster/xxxxxxxxxxxxxxxx963
+Cert Thumprint:
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXX602
+```
+
+Enjoy
+
---
+++ SignedCertWithKeyVault.ps1
@@ -0,0 +1,59 @@
+﻿# PowerShell command for ServiceFabric Test Cluster.
+# please edit the "parameter.json" 
+# Before staring this script, please Login-AzureRmAccount first.
+
+# 1. Parsing ParameterJSON
+param (
+    [string]$ParameterFile = "./parameter.json"
+    )
+$ParameterJSON = Get-Content -Raw -Path $ParameterFile | ConvertFrom-Json
+
+# 2. Creating a self-signed certificate and add it into your CertStore
+
+$password = ConvertTo-SecureString -String $ParameterJSON.SelfSignedCertificate.password -AsPlainText -Force
+New-SelfSignedCertificate -CertStoreLocation Cert:\CurrentUser\My -DnsName $ParameterJSON.SelfSignedCertificate.DnsName  -Provider 'Microsoft Enhanced Cryptographic Provider v1.0' | Export-PfxCertificate -File $ParameterJSON.SelfSignedCertificate.FileName -Password $password
+
+# 3. Creating a KeyVault
+
+New-AzureRmResourceGroup -Name $ParameterJSON.KeyVault.ResourceGroupName -Location $ParameterJSON.KeyVault.Location
+New-AzureRmKeyVault -VaultName $ParameterJSON.KeyVault.VaultName -ResourceGroupName $ParameterJSON.KeyVault.ResourceGroupName -Location $ParameterJSON.KeyVault.Location -EnabledForDeployment
+
+
+
+# 4. Adding X.509 to the KeyVault
+
+$key = Add-AzureKeyVaultKey -VaultName $ParameterJSON.KeyVault.VaultName -Name $ParameterJSON.KeyVault.KeyName -KeyFilePath $ParameterJSON.SelfSignedCertificate.FileName -KeyFilePassword $password
+
+$certfile = Get-Content -Raw -Path $ParameterJSON.SelfSignedCertificate.FileName
+$bytes = [System.IO.File]::ReadAllBytes($certfile.PSPath)
+$base64 = [System.Convert]::ToBase64String($bytes)
+
+$jsonBlob = @{
+data = $base64
+dataType = 'pfx'
+password = $ParameterJSON.SelfSignedCertificate.password
+} | ConvertTo-Json
+
+$contentBytes = [System.Text.Encoding]::UTF8.GetBytes($jsonBlob)
+$content = [System.Convert]::ToBase64String($contentBytes)
+$secretValue = ConvertTo-SecureString -String $content -AsPlainText -Force
+Set-AzureKeyVaultSecret -VaultName $ParameterJSON.KeyVault.VaultName -Name $ParameterJSON.KeyVault.KeyName -SecretValue $secretValue
+
+# 5. Getting the information
+
+
+Write-Host "---------------------------------------------------------"
+Write-Host "Done! You've got a KeyVault with Self-Signed Certificate!"
+Write-Host "---------------------------------------------------------"
+
+Write-Host "Source Key vault"
+$vault = Get-AzureRmKeyVault -VaultName $ParameterJSON.KeyVault.VaultName
+Write-Output $vault.ResourceId
+
+Write-Host "Certificate URL" 
+$vaultSecret = Get-AzureKeyVaultSecret -VaultName $ParameterJSON.KeyVault.VaultName -Name $ParameterJSON.KeyVault.KeyName
+Write-Output $vaultSecret.Id
+Write-HOST "Cert Thumprint:"
+$cert = new-object System.Security.Cryptography.X509Certificates.X509Certificate2 $certfile.PSPath, $password
+Write-Output $cert.Thumbprint
+
